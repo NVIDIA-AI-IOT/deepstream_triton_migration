019c405069c946d0e5cda8ab920d03d367f01f6e
unnik <unnik@nvidia.com>
2023-02-17 13:07:00 -0800

    Triton Migration Guide update for DS 6.2
    x86 update only (for Triton 22.09 -> later versions)
    Jetson: already latest Triton version (23.01)

--- x86_64/triton_user_additional_install_devel.sh
+++
@@ -1,4 +0,0 @@
-# additional components the user can self install
-apt-get update
-apt-get install -y gstreamer1.0-libav
-apt-get install --reinstall -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad libavresample-dev libavresample4 libavutil-dev libavutil56 libavcodec-dev libavcodec58 libavformat-dev libavformat58 libavfilter7
